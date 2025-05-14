7016315a2f602e784a55ccd9bbcd43e40d58c239
Ajay-1502 <ahdesai40@gmail.com>
2025-05-14 10:33:59 +0530

    Feature : Added Expense Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import ExpenseItem from './components/ExpenseItem.jsx';
 function App() {
   return (
     <div>
       <h1>Let's get started</h1>
-      <p>I am learning React</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
