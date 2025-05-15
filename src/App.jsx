a7af1d9851b85062f7406e59777d74cede387348
Ajay-1502 <ahdesai40@gmail.com>
2025-05-15 11:51:45 +0530

    Refactor : Added calender format to expenses

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     },
     {
       id: '4',
-      date: new Date(2025, 1, 15),
+      date: new Date(2025, 5, 15),
       location: 'Mumbai',
       title: 'Laptop',
       price: '₹50000',
