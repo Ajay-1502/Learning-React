bac7191a97475c818e5d7c69b69d79badc72d94b
Ajay-1502 <ahdesai40@gmail.com>
2025-05-19 13:13:59 +0530

    Added event handler to the button

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,12 +3,16 @@ import ExpenseDate from './ExpenseDate.jsx';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+  function buttonClickHandler() {
+    console.log('Button clicked');
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={buttonClickHandler}>Change Title</button>
       </div>
     </Card>
   );
