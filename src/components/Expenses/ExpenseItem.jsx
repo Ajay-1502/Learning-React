63444c4f487cf9f439823b2581148404d549b932
Ajay-1502 <ahdesai40@gmail.com>
2025-05-23 19:13:04 +0530

    Controlled component & lifitng state up concept to filter the expenses by year

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,21 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate.jsx';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  function buttonClickHandler() {
-    setTitle('New Title');
-  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
-        <button onClick={buttonClickHandler}>Change Title</button>
       </div>
     </Card>
   );
