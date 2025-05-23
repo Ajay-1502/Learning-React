f8f6a37528cda5305e2a4b8f260419d14115cd6f
Ajay-1502 <ahdesai40@gmail.com>
2025-05-23 19:18:49 +0530

    Controlled component & lifitng state up concept to filter the expenses by year & conditional rendering

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,13 +1,31 @@
 import './Expenses.css';
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem.jsx';
+import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 
 const Expenses = (props) => {
-  const expenses = props.expenditures || [];
+  const [filteredYear, setFilteredYear] = useState('');
+
+  const expenses = props.expenditures;
+
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = filteredYear
+    ? expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+      })
+    : expenses;
 
   return (
     <Card className="expenses">
-      {expenses.map((expense) => {
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {filteredExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
