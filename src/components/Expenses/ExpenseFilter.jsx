31e7f5b5bff830ed5c46a673c8ed34cc4e61930c
Ajay-1502 <ahdesai40@gmail.com>
2025-05-23 20:19:14 +0530

    Added one more option to dropdown

--- src/components/Expenses/ExpenseFilter.jsx
+++ src/components/Expenses/ExpenseFilter.jsx
@@ -15,6 +15,7 @@ const ExpenseFilter = (props) => {
           id="year-dropdown"
         >
           <option value="">Select the year</option>
+          <option value="2022">2022</option>
           <option value="2023">2023</option>
           <option value="2024">2024</option>
           <option value="2025">2025</option>
