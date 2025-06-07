900bdd790a192d00c1f7a67a01c5f7f5d9207814
Ajay-1502 <ahdesai40@gmail.com>
2025-06-07 23:47:04 +0530

    Context API for global state management

---
+++ src/components/React-Context Hook/LoginForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import AuthContext from './store/auth-context';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function LoginForm() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInStatus = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInStatus === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', '1');
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+    >
+      <MainHeader />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider>
+  );
+}
+
+export default LoginForm;
